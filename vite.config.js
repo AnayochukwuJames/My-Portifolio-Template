81fad28296c40018f39caa95350d3b0d53260e55
DonaldKnut <openiyiibrahim@gmail.com>
2023-09-26 23:44:18 +0100

    fixed build issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Add the FontAwesome icons to the external array
+const external = [
+  "@fortawesome/fontawesome-svg-core",
+  "@fortawesome/free-brands-svg-icons",
+];
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["@fortawesome/fontawesome-svg-core"],
+      external,
     },
   },
 });
