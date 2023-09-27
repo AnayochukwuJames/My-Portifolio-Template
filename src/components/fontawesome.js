ef97f724a7e3d472943b61983995a9e2e834ce1e
DonaldKnut <openiyiibrahim@gmail.com>
2023-09-27 01:34:16 +0100

    fixed build issue

--- src/components/fontawesome.js
+++ src/components/fontawesome.js
@@ -10,14 +10,14 @@
 // } from "@fortawesome/free-brands-svg-icons";
 
 // library.add(faFacebook, faTwitter, faInstagram);
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import {
-  faFacebook,
-  faTwitter,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+// import { library, dom } from "@fortawesome/fontawesome-svg-core";
+// import {
+//   faFacebook,
+//   faTwitter,
+//   faInstagram,
+// } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faFacebook, faTwitter, faInstagram);
+// library.add(faFacebook, faTwitter, faInstagram);
 
-// Enable automatic replacement of <i> elements with Font Awesome icons
-dom.watch();
+// // Enable automatic replacement of <i> elements with Font Awesome icons
+// dom.watch();
