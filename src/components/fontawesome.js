a2d1dd2cc6d462a78db20ded3f92394c68b3e154
DonaldKnut <openiyiibrahim@gmail.com>
2023-09-27 01:30:02 +0100

    fixed build issue

--- src/components/fontawesome.js
+++ src/components/fontawesome.js
@@ -1,6 +1,16 @@
 // fontawesome.js
 
-import { library } from "@fortawesome/fontawesome-svg-core";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { library, dom } from "@fortawesome/fontawesome-svg-core";
+
+// import {
+//   faFacebook,
+//   faTwitter,
+//   faInstagram,
+// } from "@fortawesome/free-brands-svg-icons";
+
+// library.add(faFacebook, faTwitter, faInstagram);
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import {
   faFacebook,
   faTwitter,
@@ -8,3 +18,6 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 library.add(faFacebook, faTwitter, faInstagram);
+
+// Enable automatic replacement of <i> elements with Font Awesome icons
+dom.watch();
